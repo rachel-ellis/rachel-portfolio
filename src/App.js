29137ccf2d96e85a6bc1a121525f0fc0a1615d93
rachel-ellis <rjellis@ualberta.ca>
2022-10-30 14:28:01 -0600

    feat: home page updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import rachel from './rachel.svg';
+import enggLogo from './enggLogo.svg';
 import NavBar from './components/navBar';
 import './App.css';
+
+const introText = `const intro = “Hi! My " \n + "name is Rachel Ellis”); \n` +
+`console.log($` + `{intro});`;
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <header className="App-header">
-        <img src={rachel} className="Rachel" alt="logo" />
-        <p>
-        def intro():
-          print(“Hi! My name
-          is Rachel Ellis”)
-        </p>
-      </header>
+      <div className="split left">
+        <h1>{introText}</h1>
+        <p>I’m a 4th year Software Engineering student at the University of Alberta </p>
+        <img src={enggLogo} className="logo" alt="engg logo" />
+      </div>
+      <div className="split right">
+        <img src={rachel} className="me" alt="me!" />
+      </div>
     </div>
   );
 }

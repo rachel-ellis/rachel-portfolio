5dc32730002431fac9aa1d63c9a18008fce8d662
rachel-ellis <rjellis@ualberta.ca>
2022-10-29 19:25:33 -0600

    feat: initial commit

---
+++ src/components/navBar.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import navIcon from './navIcon.svg';
+import "./navBar.css"
+
+export default function NavBar() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+    return (
+      <nav className="nav">
+        <button className="icon"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <img src={navIcon} alt="nav menu icon" />
+      </button>
+        <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
+            <ul>
+            <li>
+                <a href="/landing">Home</a>
+            </li>
+            <li>
+                <a href="/about">About Me</a>
+            </li>
+            <li>
+                <a href="/resume">Resume</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+            </ul>
+        </div>
+      </nav>
+    )
+  }
